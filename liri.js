75b73645d68d8d08b1dc3d66e2adbf22aaa8a8ac
Brice Randolph <br042393@gmail.com>
2017-08-22 11:14:21 -0700

    add option to read from file

--- liri.js
+++ liri.js
@@ -10,8 +10,33 @@ var keys = require("./keys.js");
 
  }
 
+ //first check if the user wants to enter the command from a text file
+ if (task[0]==="do-what-it-says") {
 
-if (task[0]==="my-tweets"){
+
+   //function to extract text between quotes in a string
+   function extractText( str ){
+     var ret = "";
+
+     if ( /"/.test( str ) ){
+       ret = str.match( /"(.*?)"/ )[1];
+     } else {
+       ret = str;
+     }
+
+     return ret;
+   }
+
+
+   var fs = require("fs");
+   var fileContent= fs.readFileSync("random.txt","utf8");
+   var dataArr = fileContent.split(",");
+   task = dataArr;
+   task[1] = extractText(task[1]);
+
+}
+
+ if (task[0]==="my-tweets"){
   task.splice(0,1);
   var argument = task.join(" ");
   var Twitter = require('twitter');
@@ -35,56 +60,62 @@ if (task[0]==="my-tweets"){
 
 } else if (task[0]==="spotify-this-song") {
 
-  task.splice(0,1);
-  var argument = task.join(" ");
-  //if valid input: search, else search "the sign"
-  if(!argument){
-    argument = "The Sign";
-  }
+      task.splice(0,1);
+      var argument = task.join(" ");
+      //if valid input: search, else search "the sign"
+      if(!argument){
+        argument = "The Sign";
+      }
 
 
-  var Spotify = require('node-spotify-api');
+      var Spotify = require('node-spotify-api');
 
-  var spotify = new Spotify({
-  id: "a691b8de89674f1fb619553a8d32ad2f",
-  secret: "a8c169ac2d3b4f7392983159bc7f3b60"
-  });
+      var spotify = new Spotify({
+      id: "a691b8de89674f1fb619553a8d32ad2f",
+      secret: "a8c169ac2d3b4f7392983159bc7f3b60"
+      });
 
-spotify.search({ type: 'track', query: argument }, function(err, data) {
-  if (err) {
-    return console.log('Error occurred: ' + err);
-  }
+    spotify.search({ type: 'track', query: argument }, function(err, data) {
+      if (err) {
+        return console.log('Error occurred: ' + err);
+      }
 
-console.log("Artist name: "+data.tracks.items[0].artists[0].name);
-console.log("Track name: "+data.tracks.items[0].name);
-console.log("Preview URL: "+data.tracks.items[0].preview_url);
+    console.log("Artist name: "+data.tracks.items[0].artists[0].name);
+    console.log("Track name: "+data.tracks.items[0].name);
+    console.log("Preview URL: "+data.tracks.items[0].preview_url);
 
-console.log("Album name: "+data.tracks.items[0].album.name);
+    console.log("Album name: "+data.tracks.items[0].album.name);
 
 });
 
 } else if (task[0]==="movie-this") {
 
+  task.splice(0,1);
+  var argument = task.join(" ");
   //if valid input: search, else search "the sign"
-  if(task[1]){
-    var argument = task[1];
-  } else {
-    var argument = "The sign";
+  if(!argument){
+    argument = "Mr. Nobody";
   }
 
-  // requesting data from the OMDB website
-  var request = require("request");
-  request("http://www.omdbapi.com/?t=remember+the+titans&y=&plot=short&apikey=40e9cece", function(error, response, body) {
 
-  // If there were no errors and the response code was 200 (i.e. the request was successful)
-       if (!error && response.statusCode === 200) {
+      // requesting data from the OMDB website
+      var request = require("request");
+      request("http://www.omdbapi.com/?t="+argument+"&y=&plot=short&apikey=40e9cece", function(error, response, body) {
 
-           // Then we print out the imdbRating
-           console.log("The movie's rating is: " + JSON.parse(body).imdbRating);
+      // If there were no errors and the response code was 200 (i.e. the request was successful)
+           if (!error && response.statusCode === 200) {
+
+           console.log("The movie's title is: " + JSON.parse(body).Title);
+           console.log("The movie came out in: " + JSON.parse(body).Year);
+           console.log("The movie's rating (IMDB) is: " + JSON.parse(body).imdbRating);
+           console.log("The movie's rating (Rotten Tomatoes) is: " + JSON.parse(body).Ratings[1].Value);
+           console.log("The movie was produced in: " + JSON.parse(body).Country);
+           console.log("Language: " + JSON.parse(body).Language);
+           console.log("Plot: " + JSON.parse(body).Plot);
+           console.log("Actors: " + JSON.parse(body).Actors);
+
         }
   });
 
-
-} else if (task[0]==="do-what-it-says") {
 
 }
